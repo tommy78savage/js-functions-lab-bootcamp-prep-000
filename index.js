6b6585def65aa222574cf894e460a10bf6674d77
THOMAS SAVAGE <tommy78savage@gmail.com>
2017-11-26 18:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+function happyHolidays() {
+  console.log ('Happy Holidays!')
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo (name) {
+  return 'Happy holidays, ${name}!'
+}
